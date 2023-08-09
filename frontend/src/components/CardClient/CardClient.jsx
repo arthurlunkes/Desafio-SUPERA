957e4610f00bf9b33014dd1675524f9db5cdc9b6
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-08-09 19:56:59 -0300

    Criado arquivos e pastas dos componentes.

---
+++ frontend/src/components/CardClient/CardClient.jsx
@@ -0,0 +1,10 @@
+import './CardClient.css'
+import React from 'react'
+
+const CardClient = () => {
+    return (
+        <div className="card">
+            <>nome cliente</>
+        </div>
+    )
+}
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import './Home.css'
+import React from 'react';
+
+const Home = () => {
+    return (
+        <>
+            <div className='form-backgroud'>
+
+            </div>
+            <div className='backgroud-clients'>
+
+            </div>
+
+        </>
+    );
+}
+
+export default Home
