42df27c447246252038b96234fdc3aab92a96cd6
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-08-12 20:34:09 -0300

    inserido um pouco de css

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <>
+        <div className='container'>
             <div className='form-backgroud'>
-
+                <div className="form-content">
+                    <input type="text" />
+                    
+                </div>
             </div>
             <div className='backgroud-clients'>
-
+                <p>back</p>
             </div>
 
-        </>
+        </div>
     );
 }
 
