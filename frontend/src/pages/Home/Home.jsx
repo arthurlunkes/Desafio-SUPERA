0b21aaf7ab61d3182352dd0b48feb85cb54650ea
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-08-14 21:53:17 -0300

    Inserido axios service.

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,17 +1,30 @@
 import './Home.css'
-import React from 'react';
+import React, { useState } from 'react';
+import { } from '../../services/AxiosService'
 
 const Home = () => {
+    const [name, setName] = useState('');
+    const [cnpj, setCnpj] = useState('');
+    const [clients, setClients] = useState([]);
+
     return (
         <div className='container'>
-            <div className='form-backgroud'>
-                <div className="form-content">
-                    <input type="text" />
-                    
+
+            <div className="form-content">
+                <h2>Criar Cliente</h2>
+                <div>
+                    <label htmlFor="name">Nome</label>
+                    <input id='name' type="text" />
                 </div>
+                <div>
+                    <label htmlFor="cnpj">CNPJ</label>
+                </div>
+                <div className='buttons'>
+                    <button>Salvar</button>
+                </div>
+
             </div>
             <div className='backgroud-clients'>
-                <p>back</p>
             </div>
 
         </div>
--- frontend/src/services/AxiosService.js
+++ frontend/src/services/AxiosService.js
@@ -1,1 +1,29 @@
 import axios from 'axios'
+
+axios.defaults.baseURL = 'http://localhost:8080'
+axios.defaults.headers.post["Content-Type"] = "application/json";
+
+const request = (method, url, data, headers) => {
+    return axios({
+      method: method,
+      url: url,
+      headers: headers,
+      data: data,
+    });
+  };
+
+export const getClients = () => {
+   return request('get', '/client'); 
+}
+
+export const createClient = (name, cnpj) => {
+    return request('post', '/client', {name, cnpj});
+}
+
+export const updateClient = (client) => {
+    return request('put', `/client`, client);
+}
+
+export const deleteClient = (id) => {
+    return request('delete', `/client/${id}`);
+}
