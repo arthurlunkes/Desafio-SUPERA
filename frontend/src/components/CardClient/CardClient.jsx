288f88b917de63b0941adc2141b35ea44eaa2ec6
Arthur Lunkes <arthur.lunkes2017@gmail.com>
2023-08-15 21:27:33 -0300

    Melhorado o front. layout, card, etc.

--- frontend/src/components/CardClient/CardClient.jsx
+++ frontend/src/components/CardClient/CardClient.jsx
@@ -1,10 +1,34 @@
 import './CardClient.css'
 import React from 'react'
 
-const CardClient = () => {
+const CardClient = ({Client}) => {
     return (
         <div className="card">
-            <>nome cliente</>
+
+            <div className='row header'>
+                <div className='col'>
+                    <p>Nome</p>
+                </div>
+                <div className='col'>
+                    <p>CNPJ</p>
+                </div>
+            </div>
+            
+            <div className='row'>
+                <div className='col'>
+                    <p>{Client.name}</p>
+                </div>
+                <div className="col">
+                    <p>{Client.cnpj}</p>
+                </div>
+            </div>
+
+            <div>
+                
+            </div>
+
         </div>
     )
 }
+
+export default CardClient
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,30 +1,65 @@
 import './Home.css'
-import React, { useState } from 'react';
-import { } from '../../services/AxiosService'
+import React, { useState, useEffect } from 'react';
+import Card from '../../components/CardClient/CardClient';
+import { getClients, createClient, deleteClient, updateClient } from '../../services/AxiosService'
 
 const Home = () => {
     const [name, setName] = useState('');
     const [cnpj, setCnpj] = useState('');
     const [clients, setClients] = useState([]);
 
+    useEffect(() => {
+        getClients()
+        .then(response => {
+            setClients(response.data)
+            console.log(response.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }, [])
+
     return (
         <div className='container'>
 
             <div className="form-content">
+
                 <h2>Criar Cliente</h2>
-                <div>
-                    <label htmlFor="name">Nome</label>
-                    <input id='name' type="text" />
+
+                <div className='row'>
+                    <div className='col'>
+                        <label className='form-label' htmlFor="name">Nome </label>
+                        <input
+                            id='name'
+                            class="form-label"
+                            type="text"
+                            value={name}
+                            onChange={e => setName(e.target.value)}
+                        />
+                    </div>
+
+                    <div className='col'>
+                        <label htmlFor="cnpj">CNPJ </label>
+                        <input
+                            id="cnpj"
+                            type="text"
+                            name="cnpj"
+                            value={cnpj}
+                            onChange={e => setCnpj(e.target.value)}
+                        />
+                    </div>
                 </div>
-                <div>
-                    <label htmlFor="cnpj">CNPJ</label>
-                </div>
+
                 <div className='buttons'>
-                    <button>Salvar</button>
+                    <button className="btn btn-primary" onClick={() => createClient(name, cnpj)}>Salvar</button>
                 </div>
 
             </div>
+
             <div className='backgroud-clients'>
+                {clients.map(client => (
+                    <Card key={client.id} Client={client} />
+                ))}
             </div>
 
         </div>
